734c2b737459feab4bde7e82ecf5bdd9b6a61fb1
Arya <aryaanand053@gmail.com>
2023-11-17 18:04:30 +0530

    Added security

--- ecommerce_project/ecommerce_project/settings.py
+++ ecommerce_project/ecommerce_project/settings.py
@@ -133,11 +133,3 @@ STATIC_URL = '/static/'
 MEDIA_URL = "/image/download/"
 MEDIA_ROOT = BASE_DIR
 
-# Mail
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-DEFAULT_FROM_EMAIL = 'aryaanand053@gmail.com'
